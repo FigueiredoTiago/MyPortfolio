a5acbf53f8a4e81ed256b6fe9d6035bfe6aae07d
TiagoFDeveloper <you@example.com>
2024-04-17 14:20:33 -0300

    update(music player remove)

--- scripts/audioController.js
+++
@@ -1,7 +0,0 @@
-const playButton = window.document.querySelector('.play-button');
-let audio = new Audio();
-audio.src = './assets/music1.mp3';
-
-playButton.addEventListener('click', () => { 
-    audio.play();
-} );
